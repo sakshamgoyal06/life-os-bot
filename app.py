cceb9eb7b8f1bb1766237de88c5210fe099da315
Saksham Goyal <sakshamgoyal06@gmail.com>
2025-05-23 17:18:32 +0530

    Update app.py
    Fix port issue

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import os
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
@@ -18,4 +18,6 @@ def whatsapp_reply():
     return str(resp)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
